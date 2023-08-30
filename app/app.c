d212cc698b08557f09dbb2214b3f3765ab1d5d55
PauloRSF <paulorsouzaf@gmail.com>
2023-08-29 21:33:12 -0300

    Rename app and server contexts variables

--- app/app.c
+++ app/app.c
@@ -6,79 +6,47 @@
 
 #include "app.h"
 
-AppContext ctx;
+AppContext app_ctx;
 
-void handle_ping_endpoint(ServerContext *ctx, Request request,
+void handle_ping_endpoint(ServerContext *server_ctx, Request request,
                           Response response) {
   if (request.method != GET) {
     response.status_code = 405;
-    return send_response(ctx, response);
+    return send_response(server_ctx, response);
   }
 
   response.status_code = 200;
+  response.body = "pong";
 
-  response.body = (char *)calloc(1, 5);
-  strcpy(response.body, "pong");
-
-  send_response(ctx, response);
+  send_response(server_ctx, response);
 }
 
-void handle_pessoas_endpoint(ServerContext *ctx, Request request,
-                             Response response) {
-  if (request.method != POST) {
-    response.status_code = 405;
-    return send_response(ctx, response);
-  }
-
-  Header *content_type_header = get_header(request.headers, "Content-Type");
-
-  if (content_type_header == NULL) {
-    response.status_code = 400;
-    return send_response(ctx, response);
-  }
-
-  bool has_json_body =
-      strcmp(content_type_header->value, "application/json") == 0;
-
-  free_header(content_type_header);
-
-  if (!has_json_body) {
-    response.status_code = 415;
-    return send_response(ctx, response);
-  }
-
-  response.status_code = 201;
-
-  add_header(response.headers, "Location", "/pessoas/123456d");
-
-  send_response(ctx, response);
-}
-
-void handle_request(ServerContext *ctx, Request request, Response response) {
+void handle_request(ServerContext *server_ctx, Request request,
+                    Response response) {
   log_info("%s %s", get_method_name(request.method), request.path);
 
   add_header(response.headers, "Connection", "close");
 
   if (strcmp(request.path, "/ping") == 0)
-    return handle_ping_endpoint(ctx, request, response);
+    return handle_ping_endpoint(server_ctx, request, response);
 
-  if (strcmp(request.path, "/pessoas") == 0)
-    return handle_pessoas_endpoint(ctx, request, response);
+  if (strncmp(request.path, "/pessoas", strlen("/pessoas")) == 0)
+    return handle_persons_endpoint(&app_ctx, request, response);
 
   response.status_code = 404;
 
-  send_response(ctx, response);
+  send_response(server_ctx, response);
 }
 
 void start_app(int port) {
   log_info("Starting server...");
 
-  ctx.server_context = start_server(port);
+  app_ctx.server_context = start_server(port);
 
   log_info("Listening in port %d", port);
 
   while (true) {
-    accept_connection(&ctx.server_context, handle_request);
+    accept_connection(&app_ctx.server_context, handle_request);
   }
 }
 
