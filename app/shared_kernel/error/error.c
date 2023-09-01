6b55c20b26c19f6716c96524d361e3c43d6c3a7a
PauloRSF <paulorsouzaf@gmail.com>
2023-09-01 18:06:10 -0300

    Fix error freeing bug

--- app/shared_kernel/error/error.c
+++ app/shared_kernel/error/error.c
@@ -10,14 +10,12 @@ ValidationError *create_validation_error(char *message, StringList *details) {
 
   error->base_error = (Error){.kind = ValidationErrorKind};
   error->details = StringList_clone(details);
-  error->message = NULL;
 
-  if (message != NULL) {
-    error->message = malloc(strlen(message) + 1);
-    strcpy(error->message, message);
-  } else {
-    error->message = "There were validation errors";
-  }
+  char *error_message =
+      message != NULL ? message : "There were validation errors";
+
+  error->message = malloc(strlen(error_message) + 1);
+  strcpy(error->message, error_message);
 
   return error;
 }
@@ -27,9 +25,9 @@ void free_error(Error *error) {
     return;
 
   if (error->kind == ValidationErrorKind) {
-    ValidationError *validation_error = (ValidationError *)&error;
+    ValidationError *validation_error = (ValidationError *)error;
 
-    // free(validation_error->message);
+    free(validation_error->message);
     StringList_free(validation_error->details);
   }
   free(error);
